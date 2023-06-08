b190c1f03e48d78131f8ce48e8b761716c4323af
Mimi Phan <mimiphan07@gmail.com>
2023-06-08 17:17:21 +1000

    update 2

---
+++ nice.py
@@ -0,0 +1,11 @@
+# Week 2 Lab
+# Question 1
+f = lambda x: input("Input function: ")
+l = int(input("Length of interval for x: "))
+for i in range(2):
+    temp = int(input("Initial conditions (y_0 y_L): "))
+    gamma.append(temp)
+
+#solve_bvp(l, f, gamma, P)  
+# Question 2
+# Question 3
